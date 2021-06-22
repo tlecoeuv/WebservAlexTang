b2792e48231801e5d7b984f7307b9c90630aedaf
Tanguy LECOEUVRE <tlecoeuv@e1r12p12.42.fr>
2021-06-22 14:06:37 +0200

    main corrige

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,20 +10,19 @@ void error(std::exception& ex)
 	std::cerr << "Error " << ex.what() << std::endl;
 }
 
-int main(int argc, char **argv){
-
+int main(int argc, char **argv)
+{
+	SocketManager	manager;
     if (argc != 2)
         exit(0);
     try {
         Config server(argv[1]);
 		for (size_t i = 0 ; i < server.getServer().size() ; ++i){
-			SocketManager	manager;
-
 			manager.add_server(server.getServer().at(i));
-			manager.start_servers();
 		}
     }
     catch(std::exception& ex) {
 		error(ex);
 	}
+	manager.start_servers();
 }
